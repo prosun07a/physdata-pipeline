e063e2848be0d95ccb5303c5326024e3b91b57ec
prosun07a <your-prosun07a@email.com>
2026-03-26 12:32:00 +0600

    all required installations

--- scripts/data_processor.py
+++ scripts/data_processor.py
@@ -1,1 +1,14 @@
-print("Computational Physics")
+# # age = 18.09876543
+# # x = f"I am {age :.2001234567890f} years old."
+# # print(x)
+
+# a = [1, 2, 3, 4, 5, 6, 7, [1, 2, 44], "hello orld ", "hey buddy"]
+# a[1] = 8
+# b = a[0:]
+# print(b)
+
+
+import scipy as sp
+
+a = sp.array([1, 5, 3, 3, 4, 5])
+print(a)
